55ce78e59b4785c1c5d9ab015520eb83b93703f9
Pulkit Jain <pulkitjainhk@gmail.com>
2024-05-17 02:14:24 +0530

    Created /transfer route for account and handles multiple databases transactions to be atomic Either all of them should update, or none should

--- backend/routes/account.js
+++ backend/routes/account.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const { User,Account } = require("../db");
 const authMiddleware = require("../middleware")
+const zod = require("zod");
+const mongoose = require("mongoose");
 
 router.get("/balance",authMiddleware, async (req,res) => {
     const userId = await Account.findOne({
@@ -19,14 +21,16 @@ router.get("/balance",authMiddleware, async (req,res) => {
     })
 })
 
+
 const transferSchema = zod.object({
     to: zod.string(),
     amount: zod.number()
 })
-
 router.post("/transfer",authMiddleware, async (req,res) => {
+    const session = await mongoose.startSession(); //To start session
+    session.startTransaction();
     const body = req.body;
-    const {sucess} = transferSchema.safeParse(req.body);
+    const {success} = transferSchema.safeParse(req.body);
     if(!success){
         return res.status(400).json({
             message: "Invalid Schema"
@@ -35,7 +39,6 @@ router.post("/transfer",authMiddleware, async (req,res) => {
     const account = await Account.findOne({
         dbUser : req.userId
     })
-
     if(account.balance < body.amount){
         return res.status(404).json({
             message: "Insufficient Balance"
@@ -45,16 +48,33 @@ router.post("/transfer",authMiddleware, async (req,res) => {
     const transferAccount = await Account.findOne({
         dbUser : body.to
     })
-
     if(!transferAccount){
         res.status(400).json({
             msg: "Invalid Account"
         })
     }
 
-    const updated = await Account.updateMany({
-        
+    await Account.updateOne({
+        dbUser : req.userId
+    },{
+        $inc: {
+            balance: -body.amount
+        }
     })
+    await Account.updateOne({
+        dbUser : body.to
+    },  {
+        $inc : {
+            balance: body.amount
+        }
+    }) 
+
+    // Commit the transaction
+    await session.commitTransaction();
+
+    return res.status(200).json({
+        message: "Transfer successful"
+    });
     
 })
 
